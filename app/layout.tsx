2d8f4841f2c5282940f9848b14f8922565d265fd
Sergio Yalux <randaledomman1@gmail.com>
2024-04-01 12:14:16 -0300

    feat: add theme provider and horizontal-upper-bar layout to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
+import "./globals.css";
+
 import type { Metadata } from "next";
 import { primary } from "@/app/ui/fonts";
-import GoToTop from "./ui/components/go-to-top";
-import "./globals.css";
+import GoToTop from "@/app/ui/components/go-to-top";
+import ThemeProvider from "@/app/lib/providers/theme";
+import HorizontalUpperBar from "./ui/layouts/horizontal-upper-bar";
 
 export const metadata: Metadata = {
   title: "Angela Smith",
@@ -15,9 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={primary.className}>
-        {children}
-        <GoToTop />
+      <body className={`${primary.className} bg-white text-black dark:bg-black dark:text-white`}>
+        <ThemeProvider>
+          <HorizontalUpperBar />
+          {children}
+          <GoToTop />
+        </ThemeProvider>
       </body>
     </html>
   );
